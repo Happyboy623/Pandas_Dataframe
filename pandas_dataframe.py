dc499e23aab48582ecc6348a5f0006a7926f35c3
Happyboy Mafora <74401037+Happyboy623@users.noreply.github.com>
2022-03-07 09:38:11 +0200

    Python Fundamentals

---
+++ pandas_dataframe.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.DataFrame ({'Chips':['', 'Simba', 'Lays'],'Cooldrinks':['', 'Coke', 'Fanta'],'Chocolates':['', 'Cadbury', 'Tex'],'Pies':['', 'Pepper Steak', 'Chicken'],'Fruit':['Pear', 'Apple', 'Orange'],'Cupcakes':['', 'vanilla', 'chocolate'],'Veggies':['', 'spinach', 'cabbage']})
+
+print(df)
