6176dd5af966996659512d136e31207957750284
Happyboy <happyboymafora1@gmail.com>
2022-03-28 14:23:48 +0200

    Another Commt - Sprint 3 (Week 4) Python Fundamentals

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.DataFrame ({'Chips':['', 'Simba', 'Lays'],'Cooldrinks':['', 'Coke', 'Fanta'],'Chocolates':['', 'Cadbury', 'Tex'],'Pies':['', 'Pepper Steak', 'Chicken'],'Fruit':['Pear', 'Apple', 'Orange'],'Cupcakes':['', 'vanilla', 'chocolate'],'Veggies':['', 'spinach', 'cabbage']})
+df = pd.DataFrame ({'Chips':['', 'Simba', 'Lays'],'Cooldrinks':['', 'Coke', 'Fanta'],'Chocolates':['', 'Cadbury', 'Tex'],'Pies':['', 'Pepper Steak', 'Chicken'],'Fruit':['Pear', 'Apple', 'Orange'],'Cupcakes':['', 'vanilla', 'chocolate'],'Veggies':['', 'spinach', 'cabbage'],'Salads':['Paper', 'Lettues', 'Cucamba'],'Dessert':['Custard', 'Mellows', 'Ice Cream']})
 
 print(df)
